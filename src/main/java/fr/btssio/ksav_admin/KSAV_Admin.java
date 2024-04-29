9e9694cb10368915f315a414bf876d7bdb6a54ce
Blandin Ilann <blandin.ilann@gmail.com>
2024-04-29 15:18:06 +0200

    icon

--- src/main/java/fr/btssio/ksav_admin/KSAV_Admin.java
+++ src/main/java/fr/btssio/ksav_admin/KSAV_Admin.java
@@ -3,11 +3,13 @@ package fr.btssio.ksav_admin;
 import fr.btssio.ksav_admin.controllers.Controller;
 import fr.btssio.ksav_admin.controllers.HomeController;
 import fr.btssio.ksav_admin.views.Window;
+import java.awt.Toolkit;
 
 public class KSAV_Admin {
 
     public static void main(String[] args) {
         Window window = new Window("KSAV Admin");
+        window.setIconImage(Toolkit.getDefaultToolkit().getImage(KSAV_Admin.class.getResource("/images/icon.png")));
         Controller.show(window, HomeController.class);
     }
 }
